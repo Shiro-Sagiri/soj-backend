f23c3808435378fcf398a887bc3c0297d57e49c6
Shiro <1796475983@qq.com>
2023-10-02 13:05:56 +0800

    DEBUG:修复失效的修改头像接口

--- src/main/java/com/shiro/soj/controller/UserController.java
+++ src/main/java/com/shiro/soj/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.shiro.soj.controller;
 
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.PutObjectRequest;
 import com.shiro.soj.enums.ErrorCode;
 import com.shiro.soj.common.Result;
 import com.shiro.soj.exception.BusinessException;
 import com.shiro.soj.model.dto.user.UserLoginDTO;
 import com.shiro.soj.model.dto.user.UserRegisterDTO;
 import com.shiro.soj.model.dto.user.UserUpdateDTO;
+import com.shiro.soj.model.entity.User;
 import com.shiro.soj.model.vo.UserVO;
 import com.shiro.soj.service.UserService;
 import com.shiro.soj.utils.ThreadLocalUtil;
@@ -13,10 +16,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
 
 @RestController
 @Slf4j
@@ -26,6 +34,10 @@ public class UserController {
 
     @Resource
     private UserService userService;
+    @Resource
+    private OSSClient ossClient;
+    @Value("${oss.bucket-name}")
+    private String bucketName;
 
     @PostMapping("/register")
     @ApiOperation("用户注册")
@@ -80,21 +92,42 @@ public class UserController {
     @PostMapping("/updateAvatar")
     @ApiOperation("修改头像")
     public Result<String> updateUserAvatar(@RequestBody MultipartFile file) {
-        log.info("文件上传TODO");
-        //TODO 更换版本后，文件上传功能暂时不可用
-//        String url;
-//        try {
-//            url = aliOSSUtils.upload(file);
-//        } catch (IOException e) {
-//            throw new BusinessException(ErrorCode.FILE_UPLOAD_ERROR);
-//        }
-//        User user = userService.getById(ThreadLocalUtil.getUserId());
-//        if (user == null) {
-//            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
-//        }
-//        user.setUserAvatar(url);
-//        userService.updateById(user);
-//        return Result.success(url);
-        return Result.success("接口暂时不可用");
+        log.info("文件上传");
+
+        //将MultipartFile转换成File
+        File uploadFile;
+        try {
+            String originalFilename = file.getOriginalFilename();
+            String[] filename = new String[0];
+            if (originalFilename != null) {
+                filename = originalFilename.split("\\.");
+            }
+            uploadFile = File.createTempFile(filename[0], filename[1]);
+            file.transferTo(uploadFile);
+            uploadFile.deleteOnExit();
+        } catch (IOException e) {
+            throw new BusinessException(ErrorCode.FILE_UPLOAD_ERROR);
+        }
+
+        // 创建PutObjectRequest对象:参数 bucket 和 文件名 和 文件对象
+        String filename = UUID.randomUUID() + file.getOriginalFilename();
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, filename, uploadFile);
+
+        // 上传文件。
+        ossClient.putObject(putObjectRequest);
+
+
+        //获取url
+        Date expiration = new Date(new Date().getTime() + 3600L * 1000 * 24 * 365 * 10); // 10年
+        String url = ossClient.generatePresignedUrl(bucketName, filename, expiration).toString();
+
+        //修改用户头像url
+        User user = userService.getById(ThreadLocalUtil.getUserId());
+        user.setUserAvatar(url);
+        userService.updateById(user);
+
+        // 关闭OSSClient
+        ossClient.shutdown();
+        return Result.success(url).setMessage("修改头像成功");
     }
 }
